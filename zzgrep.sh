d31e7867f37e29ed64e4a6e1904b5acd9e74838e
VikariyAmeliya <VikariyAmeliya@gmail.com>
2024-10-04 13:16:27 +0000

    New zzgrep.sh

--- zzgrep.sh
+++ zzgrep.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
-zzgrep () {
- filename="$1"
- search_pattern="$2"
-
- #Check that file is exist
-if [ ! -f "$filename" ]; then
-	echo "Failed: '$filename' doesn't exist"
-	exit 1
-fi
-
-#Creating temp directory
-script_dir="$(dirname "$0")"
-temp_dir="$script_dir"/temp
-mkdir -p "$script_dir"/temp
-
-#Unzip archive to temp	
-tar -xzf "$filename" -C "$temp_dir"
-
-#-R search with pattern
-grep -R "$search_pattern" "$temp_dir"
-#Deleting temp directory
-rm -rf "$temp_dir"
+function zzgrep {
+  mkdir -p temp 
+  tar -xzf "$1" -C temp
+  grep -R "$2" temp 
+  rm -r temp 
 }
+
+zzgrep "$@"
